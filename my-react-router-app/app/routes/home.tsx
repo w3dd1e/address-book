5e8d790a1ac7794c2df1b72726aaf63cfa082fa9
w3dd1e <kelly.weddle@gmail.com>
2024-12-17 23:47:01 -0600

    Add home and contact route pages

---
+++ my-react-router-app/app/routes/home.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+	return (
+		<>
+			<h1 id='home-title'>Address Book</h1>
+			<p id='index-page'>
+				Click a contact in the sidebar or add a new contact to get
+				started. <br />
+				<br />
+				This is a demo app for React Router 7. <br /> <br />
+				Check out{' '}
+				<a href='https://reactrouter.com'>
+					the docs at reactrouter.com
+				</a>
+				.
+			</p>
+		</>
+	);
+}
