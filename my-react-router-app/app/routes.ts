842bc29ae5c435f228b024b15263e0d43b4801d2
w3dd1e <kelly.weddle@gmail.com>
2024-12-17 23:45:22 -0600

    Add index and contact routes

--- my-react-router-app/app/routes.ts
+++ my-react-router-app/app/routes.ts
@@ -1,3 +1,7 @@
-import type { RouteConfig } from "@react-router/dev/routes";
+import type { RouteConfig } from '@react-router/dev/routes';
+import { index, route } from '@react-router/dev/routes';
 
-export default [] satisfies RouteConfig;
+export default [
+	index('routes/home.tsx'),
+	route('contacts/:contactId', 'routes/contact.tsx'),
+] satisfies RouteConfig;
